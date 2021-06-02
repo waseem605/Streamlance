c615f7dddd4ebf4dca1646a303157b3d7e14d485
Huzaifa Sammar <hm101326@gmail.com>
2021-06-02 15:32:38 +0500

    Adapter

---
+++ app/src/main/java/com/example/streamlance/Adapter/FragementAdapter.java
@@ -0,0 +1,17 @@
+package com.example.streamlance.Adapter;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.example.streamlance.Fragments.Home;
+import com.example.streamlance.Fragments.Notification;
+import com.example.streamlance.Fragments.Profile;
+import com.example.streamlance.Fragments.Search;
+
+public class FragementAdapter {
+
+}
