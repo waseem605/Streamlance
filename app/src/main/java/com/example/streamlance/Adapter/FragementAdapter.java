45006993bd06b935e1b1a653d8d7a08535dfc0c2
Huzaifa Sammar <hm101326@gmail.com>
2021-06-06 15:37:54 +0500

    fragment

--- app/src/main/java/com/example/streamlance/Adapter/FragementAdapter.java
+++ app/src/main/java/com/example/streamlance/Adapter/FragementAdapter.java
@@ -1,17 +1,66 @@
 package com.example.streamlance.Adapter;
 
 
+import android.renderscript.ScriptGroup;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.example.streamlance.Fragments.Completed;
 import com.example.streamlance.Fragments.Home;
 import com.example.streamlance.Fragments.Notification;
 import com.example.streamlance.Fragments.Profile;
 import com.example.streamlance.Fragments.Search;
+import com.example.streamlance.Fragments.my_booking;
+import com.example.streamlance.Fragments.mygarage;
 
-public class FragementAdapter {
+import org.jetbrains.annotations.NotNull;
 
+public class FragementAdapter extends FragmentPagerAdapter {
+
+    public FragementAdapter(@NonNull @NotNull FragmentManager fm) {
+        super(fm);
+    }
+
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new mygarage();
+            case 1: return new my_booking();
+            case 2: return new Completed();
+            default:return new mygarage();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title=null;
+        if(position==0)
+        {
+            title="MY GARAGE";
+        }
+        if(position==1)
+        {
+            title="MY BOOKINGS";
+        }
+        if(position==2)
+        {
+            title="COMPLETED";
+        }
+        return title;
+    }
 }
+
--- app/src/main/java/com/example/streamlance/Fragments/Home.java
+++ app/src/main/java/com/example/streamlance/Fragments/Home.java
@@ -2,16 +2,28 @@ package com.example.streamlance.Fragments;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
+import com.example.streamlance.Adapter.FragementAdapter;
 import com.example.streamlance.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.zip.Inflater;
 
 
 public class Home extends Fragment {
 
-
+    FragementAdapter fragementAdapter;
+    private TabLayout mtabLayout;
+    private ViewPager mviewPager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,10 +31,19 @@ public class Home extends Fragment {
 
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+       View v=inflater.inflate(R.layout.fragment_home, container, false);
+         mtabLayout=(TabLayout) v.findViewById (R.id.tablayout);
+         mviewPager=(ViewPager) v.findViewById(R.id.viewPage);
+         fragementAdapter=new FragementAdapter(getActivity().getSupportFragmentManager());
+         mviewPager.setAdapter(fragementAdapter);
+         mtabLayout.setupWithViewPager(mviewPager);
+
+       return v;
+
     }
 }
