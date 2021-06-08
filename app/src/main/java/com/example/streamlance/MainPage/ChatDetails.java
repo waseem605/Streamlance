746e18ac6bca6744e1abb9c91a83bc3dc28e12cc
waseem605 <w.shahzad605@gmail.com>
2021-06-08 22:04:47 +0500

    Appbar sybservices

---
+++ app/src/main/java/com/example/streamlance/MainPage/ChatDetails.java
@@ -0,0 +1,16 @@
+package com.example.streamlance.MainPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.streamlance.R;
+
+public class ChatDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_details);
+    }
+}
