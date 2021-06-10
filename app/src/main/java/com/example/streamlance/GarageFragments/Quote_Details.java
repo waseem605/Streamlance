5aaf61ce1323a371ed655583a2e349cb6fcc5a01
Huzaifa Sammar <hm101326@gmail.com>
2021-06-10 18:54:02 +0500

    ProvidersQuotes

---
+++ app/src/main/java/com/example/streamlance/GarageFragments/Quote_Details.java
@@ -0,0 +1,16 @@
+package com.example.streamlance.GarageFragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.streamlance.R;
+
+public class Quote_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote_details);
+    }
+}
--- app/src/main/java/com/example/streamlance/MainPage/ProvidersQuotes.java
+++ app/src/main/java/com/example/streamlance/MainPage/ProvidersQuotes.java
@@ -2,15 +2,28 @@ package com.example.streamlance.MainPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.streamlance.GarageFragments.Garage_Info;
 import com.example.streamlance.R;
 
 public class ProvidersQuotes extends AppCompatActivity {
+    Button mbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_providers_quotes);
+        mbtn=(Button)findViewById(R.id.viewquotes);
+        mbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ProvidersQuotes.this, Garage_Info.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
