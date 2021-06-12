e8f6c5c4c0ab466aed7d901e861e0a1c83e6b243
Huzaifa Sammar <hm101326@gmail.com>
2021-06-12 16:21:01 +0500

    payment methods

---
+++ app/src/main/java/com/example/streamlance/Payments/ReviewAndPay.java
@@ -0,0 +1,16 @@
+package com.example.streamlance.Payments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.streamlance.R;
+
+public class ReviewAndPay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_and_pay);
+    }
+}
