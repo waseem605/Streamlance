48667b4447a50a5e665dbcc45e619bda2e73db58
waseem605 <w.shahzad605@gmail.com>
2021-06-12 16:20:53 +0500

    add vehical garage info font sort bar flow2 complete

---
+++ app/src/main/java/com/example/streamlance/FlowTwo/DiagnosisReport.java
@@ -0,0 +1,35 @@
+package com.example.streamlance.FlowTwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.streamlance.R;
+import com.example.streamlance.Services.Services_Summary;
+
+public class DiagnosisReport extends AppCompatActivity {
+
+    private Button mConfirmBtn,mViewDiagonosBtn;
+    Dialog request_dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnosis_report);
+
+        mConfirmBtn = (Button)findViewById(R.id.confirm_request_diagonos);
+        mConfirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(DiagnosisReport.this);
+                dialog.setContentView(R.layout.request_submitted_dialog);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                //mViewDiagonosBtn = (Button)dialog.findViewById(R.id.view_diagonos_quota_btn);
+                dialog.show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/streamlance/FlowTwo/Diagonosis.java
+++ app/src/main/java/com/example/streamlance/FlowTwo/Diagonosis.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.streamlance.Fragments.Home;
 import com.example.streamlance.Fragments.Notification;
@@ -18,11 +22,59 @@ import org.jetbrains.annotations.NotNull;
 
 public class Diagonosis extends AppCompatActivity {
 
+    private Button mRequestbtn;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagonosis);
+        mRequestbtn = (Button)findViewById(R.id.request_btn_D);
+        linearLayout = findViewById(R.id.diagonos_layout_mini);
+
+
+        mRequestbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Diagonosis.this,DiagnosisReport.class));
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_main_navigation);
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Home()).commit();
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.home_nav:
+                        linearLayout.setVisibility(View.INVISIBLE);
+                        fragment = new Home();
+                        break;
+
+                    case R.id.search_nav:
+                        linearLayout.setVisibility(View.INVISIBLE);
+                        fragment = new Search();
+                        break;
+
+                    case R.id.notification_nav:
+                        linearLayout.setVisibility(View.INVISIBLE);
+                        fragment = new Notification();
+                        break;
+
+                    case R.id.user_nav:
+                        linearLayout.setVisibility(View.INVISIBLE);
+                        fragment = new Profile();
+                        break;
+                }
+                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.diagonos_layout, fragment).commit();
+                return true;
+            }
+        });
 
 
     }
